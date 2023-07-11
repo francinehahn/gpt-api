44c6f5740ff9b19dbc894f5728607345c03570a5
Francine Hahn <fran_hahn@hotmail.com>
2023-07-11 11:56:40 -0300

    feat: I included error handling in the summary endpoint

--- src/endpoints/summary.py
+++ src/endpoints/summary.py
@@ -10,20 +10,28 @@ bp = Blueprint('summary', __name__)
 
 @bp.route("/summary", methods=["POST"])
 def summary():
-    body = request.json
+    try:
+        body = request.json
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Resuma o seguinte texto: {body['text']}",
-        temperature=0.8,
-        max_tokens=2048,
-        n=1,
-        stop=None
-    )
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Resuma o seguinte texto: {body['text']}",
+            temperature=0.8,
+            max_tokens=2048,
+            n=1,
+            stop=None
+        )
 
-    response = jsonify(
-        message = "Here is the summary of the text:",
-        data = response['choices'][0]['text']
-    )
-    response.status_code = 201
-    return response
+        response = jsonify(
+            message = "Aqui está o resumo do texto:",
+            data = response['choices'][0]['text']
+        )
+        response.status_code = 201
+        return response
+    
+    except Exception as err:
+        response = jsonify(
+            message = f"Unexpected error: {err}"
+        )
+        response.status_code = 400
+        return response
