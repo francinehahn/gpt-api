ab1e5f7b479cc6be5f56c894b77e5ee0728b1aee
Francine Hahn <fran_hahn@hotmail.com>
2023-07-11 11:57:49 -0300

    feat: I included error handling in the writing assistant endpoint

--- src/endpoints/writing_assistant.py
+++ src/endpoints/writing_assistant.py
@@ -10,21 +10,29 @@ bp = Blueprint('writing-assistant', __name__)
 
 @bp.route("/writing-assistant", methods=["POST"])
 def writing_assistant():
-    body = request.json
+    try:
+        body = request.json
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Crie um texto para: {body['text']}",
-        temperature=0.8,
-        max_tokens=2048,
-        n=1,
-        stop=None
-    )
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Crie um texto para: {body['text']}",
+            temperature=0.8,
+            max_tokens=2048,
+            n=1,
+            stop=None
+        )
 
-    response = jsonify(
-        message = "Aqui está o texto que você pediu:",
-        data = response['choices'][0]['text']
-    )
-    response.status_code = 201
-    return response
+        response = jsonify(
+            message = "Aqui está o texto que você pediu:",
+            data = response['choices'][0]['text']
+        )
+        response.status_code = 201
+        return response
+    
+    except Exception as err:
+        response = jsonify(
+            message = f"Unexpected error: {err}"
+        )
+        response.status_code = 400
+        return response
     
