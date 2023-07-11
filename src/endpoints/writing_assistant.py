8893e09666e6233e92f38e4d2c47fc638dca5051
Francine Hahn <fran_hahn@hotmail.com>
2023-07-11 11:44:18 -0300

    feat: I wrote the writing assistant endpoint

---
+++ src/endpoints/writing_assistant.py
@@ -0,0 +1,30 @@
+from flask import jsonify, request, Blueprint
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+bp = Blueprint('writing-assistant', __name__)
+
+@bp.route("/writing-assistant", methods=["POST"])
+def writing_assistant():
+    body = request.json
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Crie um texto para: {body['text']}",
+        temperature=0.8,
+        max_tokens=2048,
+        n=1,
+        stop=None
+    )
+
+    response = jsonify(
+        message = "Aqui está o texto que você pediu:",
+        data = response['choices'][0]['text']
+    )
+    response.status_code = 201
+    return response
+    
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from endpoints.summary import bp as bp_summary
-
+from endpoints.writing_assistant import bp as bp_writing_assistant
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
 
 
 app.register_blueprint(bp_summary)
+app.register_blueprint(bp_writing_assistant)
 
 
 if __name__ == "__main__":
