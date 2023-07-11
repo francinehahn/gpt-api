3d7a27f3242175c514689a9d276c882e899dd5d0
Francine Hahn <fran_hahn@hotmail.com>
2023-07-11 11:54:29 -0300

    feat: I wrote the translator endpoint

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from endpoints.summary import bp as bp_summary
 from endpoints.writing_assistant import bp as bp_writing_assistant
+from endpoints.translator import bp as bp_translator
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
@@ -8,6 +9,7 @@ app.config["JSON_SORT_KEYS"] = False
 
 app.register_blueprint(bp_summary)
 app.register_blueprint(bp_writing_assistant)
+app.register_blueprint(bp_translator)
 
 
 if __name__ == "__main__":
