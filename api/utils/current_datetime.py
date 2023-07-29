9b1c7e7b6270502fae378e9b170a44367f241251
Francine Hahn <fran_hahn@hotmail.com>
2023-07-29 15:32:27 -0300

    fix: I fixed the current datetime function

--- api/utils/current_datetime.py
+++ api/utils/current_datetime.py
@@ -1,9 +1,10 @@
 """Current Datetime"""
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def current_time():
-    """Function that returns the current datetime in the format 2023-07-29 14:29:49"""
-    datetime_now = datetime.now()
+    """Function that returns the datetime in the format 2023-07-29 14:29:49 
+    (3 hours less than the current time to be added to the database correctly)"""
+    datetime_now = datetime.now() - timedelta(hours=3)
     datetime_format = "%Y-%m-%d %H:%M:%S"
     datetime_edited = datetime_now.strftime(datetime_format)
     return datetime_edited
