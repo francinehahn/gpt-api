4fb93c28fe0e42393fff9d9ff52d36e1428017b2
Francine Hahn <fran_hahn@hotmail.com>
2023-07-30 17:53:39 -0300

    feat: I installed and added cors

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@
 from datetime import timedelta
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv, find_dotenv
 
@@ -10,5 +11,6 @@ load_dotenv(find_dotenv())
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
+CORS(app)
 
 jwt = JWTManager(app)
